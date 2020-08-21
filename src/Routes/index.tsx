e131b6ae2ff7e6adf94d6ed091af963ec344190d
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-21 20:55:12 -0300

    Create MoviePage route

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import MoviePage from '../pages/MoviePage';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route path='/movie' component={MoviePage} />
     </Switch>
   )
 }
