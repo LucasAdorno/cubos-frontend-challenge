22fac59e694f01be2c2ccddf5295f2f291ee8b2f
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-21 20:53:46 -0300

    Header styles

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  background: #116193;
+  color: #00e8e4;
+
+  .header-title{
+    font-weight: 400;
+    font-size: 40px;
+  }
+
+`;
