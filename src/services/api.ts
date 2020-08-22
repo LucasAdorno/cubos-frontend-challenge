087a04181c79fba2813da1c94db8658953f918cb
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-22 13:17:27 -0300

    Movies details api add

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,10 @@ const api = axios.create({
 
 const api_key = "89982a4f09932ccfb4818f10692c02f2";
 
-export const ListMovies = (query: string) => {
+export const listMovies = (query: string) => {
   return api.get(`/search/movie?api_key=${api_key}&language=pt-BR&query=${query}`)
+};
+
+export const detailsMovie = (id: string) => {
+  return api.get(`/movie/${id}?api_key=89982a4f09932ccfb4818f10692c02f2&language=pt-BR`)
 };
