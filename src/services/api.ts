e441bf1779cf8e8bf5e8f8836bbca1719c7268fe
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-22 01:16:55 -0300

    Set api params

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/"
-})
+  baseURL: "https://api.themoviedb.org/3"
+});
 
-export default api;
+const api_key = "89982a4f09932ccfb4818f10692c02f2";
+
+export const ListMovies = (query: string) => {
+  return api.get(`/search/movie?api_key=${api_key}&language=pt-BR&query=${query}`)
+};
