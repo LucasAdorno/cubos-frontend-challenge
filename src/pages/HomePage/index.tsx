d906caa96fbc68a8984bb47e0eb10f72a7e0280a
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-22 20:31:03 -0300

    Add input

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+import { listMovies } from '../../services/api'
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const HomePage: React.FC = () => {
-  return <h1>hello cubos!</h1>;
+
+  interface Dados {
+    title: string,
+  }
+
+  const [dados, setDados] = useState<[Dados]>([{ title: 'nada' }])
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    listMovies(e.target.value).then(response => {
+      setDados(response.data.results)
+    });
+  }
+
+  return (
+    <Container id='homepage-content'>
+      <input id='search-input' 
+        type='text'
+        placeholder='Busque um filme por nome, ano ou gênero...'
+        onChange={(e) => handleInput(e)}
+      />
+    </Container>
+  );
 }
 
 export default HomePage;
