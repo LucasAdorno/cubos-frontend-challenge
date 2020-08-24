67b131951a8fbbc5a18c90df0d7b4f4e29705140
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-23 23:22:59 -0300

    Paginate style

--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -4,9 +4,19 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 88%;
+  margin: 0 auto;
+  
+  #homepage-content {
+    min-height: 74vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
 
   #search-input{
-    width: 80%;
+    width: 100%;
     height: 50px;
     padding: 0 20px 0 20px;
     margin: 40px 0 40px 0;
@@ -21,4 +31,41 @@ export const Container = styled.div`
     color: #7eafce;
   }
 
+  #pagination{
+    display: flex;
+    align-items: center;
+    margin: 20px 0 20px 0;
+  }
+
+  #pagination .active {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 46px;
+    height: 46px;
+    background: #00e8e4;
+    border: 2px solid #116193;
+    border-radius: 50%;
+  }
+
+  .border-marker .page-marker {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    padding-bottom: 2px;
+    border-radius: 50%;
+    background: none;
+    color: #116193;
+    font-size: 20px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+  .active .page-marker {
+    background: #116193;
+    color: #00e8e4;
+    font-size: 28px;
+  }
 `;
