1f8fa29225216015313afdea1b2ba8361e195dab
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-25 10:31:11 -0300

    Margin ajusts

--- src/components/GenreList/styles.ts
+++ src/components/GenreList/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Genre = styled.p`
   padding: 2px 10px;
-  margin: 14px 8px 30px 0;
+  margin: 8px 8px 0 0;
   border: 1px solid  #116193;
   color: #116193;
   border-radius: 18px;
