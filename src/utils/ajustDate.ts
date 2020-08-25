a1f8bdd6947b917d227d006623dee7ad3a14f181
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-25 10:38:45 -0300

    Check date

--- src/utils/ajustDate.ts
+++ src/utils/ajustDate.ts
@@ -1,5 +1,5 @@
 const ajustDate = (date: string): string => {
-  if(date[0] !== undefined) {
+  if(date) {
     let splitDate = date.split('-');
     let resolveDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
     return resolveDate
